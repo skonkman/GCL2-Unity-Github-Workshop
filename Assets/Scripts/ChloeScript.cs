bfe828fe01d9b803efef175a527014609234e891
kooconut <kooknuut@gmail.com>
2026-04-29 11:36:40 +0800

    [Chloe] Add Sprite, script and Dave the magical cheese wizard game object

---
+++ Assets/Scripts/ChloeScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Davethemagicalcheesewizard : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("I am Dave the Magical Cheese Wizard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
