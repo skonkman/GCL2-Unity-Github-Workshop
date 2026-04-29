de3139de520deb5978e9bcf34ab337c24241702b
Mister-MU <reigneandres2008@gmail.com>
2026-04-29 10:52:52 +0800

    Reigne
    - Added Sprite + collider
    - Added 'ground' + collider
    - Added simple script

---
+++ Assets/Scripts/ReigneScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ReigneScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
